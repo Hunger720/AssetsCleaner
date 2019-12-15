1b56d9f229ed0f4df76bb426544b3fbcaaa11d48
Hunger720 <Hunger720@gmail.com>
2019-12-15 14:58:58 +0800

    放到Editor目录

---
+++ Assets/Editor/AssetsCleaner.cs
@@ -0,0 +1,47 @@
+﻿// AssetsCleaner.cs
+// 废弃资源的查找和清除
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetsCleaner
+{
+    [MenuItem("AssetsCleaner/查找所有废弃资源")]
+    public static void FindAbandonedAssets()
+    {
+        // 列出所有prefab
+        string path = Application.dataPath + "/UI/Windows";
+        List<string> dirs = new List<string>();
+        GetDirs(path, ref dirs);
+    }
+
+    private static void GetDirs(string dirPath, ref List<string> dirs)
+    {
+        foreach (string path in Directory.GetFiles(dirPath))
+        {
+            //获取所有文件夹中包含后缀为 .prefab 的路径
+            if (System.IO.Path.GetExtension(path) == ".prefab")
+            {
+                string p = path.Substring(path.IndexOf("Assets"));
+                dirs.Add(p);
+                Debug.Log(p);
+
+                string[] dependencies = AssetDatabase.GetDependencies(p);
+                foreach(string d in dependencies)
+                {
+                    Debug.Log(d);
+                }
+            }
+        }
+ 
+        foreach (string path in Directory.GetDirectories(dirPath))
+        {
+            GetDirs(path, ref dirs);
+        }
+    }
+
+}
